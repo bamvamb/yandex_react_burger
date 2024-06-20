4712989e0b20f17e77a965312531a70e369c8fd6
bamvamb <racott.nick@gmail.com>
2024-06-21 01:02:44 +0300

    add loader and error-view to components

--- src/components/pages/ingredient/index.tsx
+++ src/components/pages/ingredient/index.tsx
@@ -1,12 +1,14 @@
 import {useParams} from 'react-router-dom'
 import { useIngredientDetailsQuery } from "../../../services/apis/data"
 import IngredientDetail from '../../ingredient-details/ingredient-details';
+import Loader from '../../share/loader/loader';
+import ErrorView from '../../share/error/error';
 
 const IngredientPage = () => {
     const { id } = useParams();
     const { data:ingredient, isLoading } = useIngredientDetailsQuery({ingredient_id:id})
-    if( isLoading ) return <div>Загружаю данные...</div>
-    if (!ingredient) return <div>Ингридиента не существует</div>
+    if( isLoading ) return <Loader text="Загружаю данные..."/>
+    if (!ingredient) return <ErrorView text="Ингридиента не существует"/>
     return <IngredientDetail ingredient={ingredient}/>
 }
 
